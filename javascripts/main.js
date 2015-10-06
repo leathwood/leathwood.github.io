2e7bb942779063d60847ab2b9d4155593ed044af
Leathwood <paul.leathwood@gmail.com>
2015-10-06 16:22:25 +0100

    =Initial work

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+jQuery(window).load(function () {
+  jQuery("#loaderInner").fadeOut();
+  jQuery("#loader").delay(400).fadeOut("slow");
+});
+
+$(function () {
+  "use strict";
+  $('#worksGrid').mixItUp();
+});
+
+$('body').scrollspy({
+  offset: 200, target: '.navigation'
+});
